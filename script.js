79226d2d789d5bb4b0a4f186903bf2cd04152c99
Partho <fid.cast.3321@gmail.com>
2021-09-12 18:06:07 +0600

    some changes

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 "use strict";
-// console.log(document.querySelector('.message').textContent)
-// document.querySelector('.number').textContent = 13;
-// document.querySelector('.score').textContent = 20;
-// document.querySelector('.guess').value = 23;
 let secretNumber = Math.trunc(Math.random() * 20 + 1);
 let score = 20;
 let highScore = 0;
